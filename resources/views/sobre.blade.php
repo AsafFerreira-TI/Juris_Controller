17be669261d3345ef52be28b0c44e0ad918150c8
kauakaw <kaua.lade14@gmail.com>
2026-09-01 12:41:10 +0000

    tchau

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -29,5 +29,27 @@
     </div>
 
 </section>
+<section class="sobre">
 
+    <div class="sobre-conteudo">
+
+        <span class="sobre-label">
+            SOBRE NÓS
+        </span>
+
+        <h1>
+            Tecnologia e estratégia<br>
+            para escritórios que<br>
+            querem <span>ir além.</span>
+        </h1>
+
+        <p>
+            O Juris Control nasceu com o propósito de transformar
+            a gestão jurídica por meio da tecnologia, da inovação e
+            do compromisso com resultados.
+        </p>
+
+    </div>
+
+</section>
 @endsection
